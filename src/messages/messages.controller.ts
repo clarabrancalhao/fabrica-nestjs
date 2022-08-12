4f0e8a7b39369249a52d1a5039d94c99c482741d
Clara Brancalhão <brancalhaoclara@gmail.com>
2022-08-11 23:58:59 -0300

    feat: create messages controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  findAll() {
+    return [
+      {
+        id: 1,
+        message: 'Hello World!',
+      },
+      {
+        id: 2,
+        message: 'Hello World!',
+      },
+    ];
+  }
+}
