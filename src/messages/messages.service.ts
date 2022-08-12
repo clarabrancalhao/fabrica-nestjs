98811d78512a552a366ee165d3823df3c33fd8cf
Clara Brancalhão <brancalhaoclara@gmail.com>
2022-08-12 01:04:12 -0300

    feat: add not found error to findById messages endpoint

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -18,8 +18,14 @@ export class MessagesService {
     return this.messages;
   }
 
-  findById(id: number) {
-    return this.messages.find((message) => message.id === id);
+  async findById(id: number) {
+    const message = this.messages.find((message) => message.id === id);
+
+    if (!message) {
+      throw new Error('Message not found');
+    }
+
+    return message;
   }
 
   create(message: { id: number; text: string }) {
