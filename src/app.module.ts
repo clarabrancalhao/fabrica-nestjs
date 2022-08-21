0015842aedac0e15ed6f902bbadcfabde833c94b
Clara Brancalhão <brancalhaoclara@gmail.com>
2022-08-21 18:03:22 -0300

    feat: add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [PokemonModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
